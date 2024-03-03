fcf3b8337e9f5904d0fcee68bab6a2b9a516f876
daylightcoderj6 <jananibharathi6@gmail.com>
2024-03-03 20:37:10 +0530

    added callback hell function

---
+++ src/script.js
@@ -0,0 +1,36 @@
+    setTimeout(()=>{
+       document.querySelector(".countDown").innerText = "10"
+       setTimeout(()=>{
+        document.querySelector(".countDown").innerText = "9"
+        setTimeout(()=>{
+            document.querySelector(".countDown").innerText = "8"
+            setTimeout(()=>{
+                document.querySelector(".countDown").innerText = "7"
+                setTimeout(()=>{
+                    document.querySelector(".countDown").innerText = "6"
+                    setTimeout(()=>{
+                        document.querySelector(".countDown").innerText = "5"
+                        setTimeout(()=>{
+                            document.querySelector(".countDown").innerText = "4"
+                            setTimeout(()=>{
+                                document.querySelector(".countDown").innerText = "3"
+                                setTimeout(()=>{
+                                    document.querySelector(".countDown").innerText = "2"
+                                    setTimeout(()=>{
+                                        document.querySelector(".countDown").innerText = "1"
+                                        setTimeout(()=>{
+                                            document.querySelector(".countDown").innerText = "Happy Independence Day"
+                                            
+                                         },1000)
+                                     },1000)
+                                 },1000)
+                             },1000)
+                         },1000)
+                     },1000)
+                    
+                 },1000)  
+             },1000)
+         },1000)
+     },1000)
+    },1000)
+
